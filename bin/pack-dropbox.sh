dcc62631cd8a07ae924fd00b5ab21f5c9a133ae9
lainme <lainme993@gmail.com>
2012-09-28 10:51:38 +0800

    exclude more from packaging dropbox

--- bin/pack-dropbox.sh
+++ bin/pack-dropbox.sh
@@ -5,6 +5,6 @@ cd $HOME
 
 cp $HOME/bin/arch-post.sh $HOME/
 
-tar --exclude="home/config/.gnupg/*" --exclude="home/config/.ssh/*" --exclude="home/config/.irssi/*" --exclude="home/config/.config/fcitx/*" --exclude="home/config/.purple/*" --exclude="home/config/.vim/*" --exclude="home/config/.git" --exclude="home/reference/*" --exclude="home/Documents/*" --exclude="home/Music/*" --exclude="home/Pictures/wallpaper" --exclude="Public" --exclude="exchange" --exclude=".dropbox.cache" --exclude=".dropbox" --exclude="sysconf/dropbox.tar.gz" -cpzf $HOME/Dropbox/sysconf/dropbox.tar.gz Dropbox arch-post.sh
+tar --exclude="home/config/.gnupg/*" --exclude="home/config/.ssh/*" --exclude="home/config/.irssi/*" --exclude="home/config/.config/fcitx/*" --exclude="home/config/.purple/*" --exclude="home/config/.vim/*" --exclude="home/config/.git" --exclude="home/reference/*" --exclude="home/Documents/*" --exclude="home/Music/*" --exclude="home/Pictures/wallpaper" --exclude="Public" --exclude="exchange" --exclude="develop" --exclude="cluster" --exclude="server" --exclude=".dropbox.cache" --exclude=".dropbox" --exclude="sysconf/dropbox.tar.gz" -cpzf $HOME/Dropbox/sysconf/dropbox.tar.gz Dropbox arch-post.sh
 
 rm $HOME/arch-post.sh
