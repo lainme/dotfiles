d147e6277be7419e2540f563a82b612ccdd8f467
lainme <lainme993@gmail.com>
2014-12-16 12:50:03 +0800

    fix comparision

--- bin/copystorage.sh
+++ bin/copystorage.sh
@@ -3,8 +3,8 @@
 MOZF=`ls $HOME/.mozilla/firefox/*.default/webappsstore.sqlite`
 BAKF=$HOME/Dropbox/home/Documents/webappsstore.sqlite
 
-if [ "$1"=="update" ];then
+if [ "$1" == "update" ];then
     cp $BAKF $MOZF
-elif [ "$1"=="backup" ];then
+elif [ "$1" == "backup" ];then
     cp $MOZF $BAKF
 fi
