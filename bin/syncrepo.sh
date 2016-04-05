0cc44c717aebce52e35e739b39f692543ae86100
lainme <lainme993@gmail.com>
2016-04-05 11:07:07 +0800

    update syncrepo

--- bin/syncrepo.sh
+++ bin/syncrepo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "--send" ];then
-    sudo rsync -rv --delete /var/lib/pacman/sync/   /run/media/lainme/Linux/pacman/sync/
-    sudo rsync -rv --delete /var/cache/pacman/pkg/  /run/media/lainme/Linux/pacman/pkg/
+    sudo rsync -av --delete /var/lib/pacman/sync/   /run/media/lainme/Linux/pacman/sync/
+    sudo rsync -av --delete /var/cache/pacman/pkg/  /run/media/lainme/Linux/pacman/pkg/
     sudo rsync -av --delete /home/lainme/Dropbox/   /run/media/lainme/Linux/Dropbox/
 elif [ "$1" = "-receive" ];then
-    sudo rsync -rv --delete /run/media/lainme/Linux/pacman/sync/    /var/lib/pacman/sync/
-    sudo rsync -rv --delete /run/media/lainme/Linux/pacman/pkg/     /var/cache/pacman/pkg/
+    sudo rsync -av --delete /run/media/lainme/Linux/pacman/sync/    /var/lib/pacman/sync/
+    sudo rsync -av --delete /run/media/lainme/Linux/pacman/pkg/     /var/cache/pacman/pkg/
     sudo rsync -av --delete /run/media/lainme/Linux/Dropbox/        /home/lainme/Dropbox/
 fi
