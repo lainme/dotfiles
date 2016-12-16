af88e7556496e289dd9042481b32b517eb12fd8c
lainme <lainme993@gmail.com>
2016-12-16 22:20:58 +0800

    add symlink

--- bin/autoinstall.sh
+++ bin/autoinstall.sh
@@ -121,12 +121,13 @@ function setup_usrconf(){
     $RUNASUSR xdg-user-dirs-update
 
     # symbol link
-    helper_symlink $USERHOME/Dropbox/home $USERHOME "/(\.config$|\.local$|\.cow$|\.ssh$|\.sage$|\.git$|\.gitignore$|sysconf$)/d;p"
+    helper_symlink $USERHOME/Dropbox/home $USERHOME "/(\.config$|\.local$|\.cow$|\.ssh$|\.sage$|\.git$|\.gitignore$|sysconf$|intel$)/d;p"
     helper_symlink $USERHOME/Dropbox/home/.config       $USERHOME/.config
     helper_symlink $USERHOME/Dropbox/home/.cow          $USERHOME/.cow
     helper_symlink $USERHOME/Dropbox/home/.sage         $USERHOME/.sage
     helper_symlink $USERHOME/Dropbox/home/.local/share  $USERHOME/.local/share
     helper_symlink $USERHOME/Dropbox/home/.ssh          $USERHOME/.ssh
+    helper_symlink $USERHOME/Dropbpx/home/intel         $USERHOME/intel
 
     # avatar
     cp $USERHOME/Dropbox/home/sysconf/account/avatar-gnome.png /var/lib/AccountsService/icons/$USERNAME
