4d923dd97fb23bcec03accd4c7059f5277b2d966
lainme <lainme993@gmail.com>
2017-01-13 13:57:55 +0800

    add ddns script

---
+++ bin/ddns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURIP=$(cat $HOME/.ddns)
+NEWIP=$(curl -s ipinfo.io/ip)
+if [ $CURIP == $NEWIP ];then
+    exit 0
+fi
+if [ -z $NEWIP ];then
+    exit 0
+fi
+echo $NEWIP > $HOME/.ddns
+
+sed -i -e "/lainme-home-desktop/{N;s/[0-9.]\+/$NEWIP/}" $HOME/.ssh/config
